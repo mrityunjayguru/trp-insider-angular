2395cfd7dc9c55761c5d5d588a9765485445456c
nirajniit <niit.delhi.ind@gmail.com>
2026-02-04 13:56:58 +0530

    commit

--- src/app/apiservice.spec.ts
+++ src/app/apiservice.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Apiservice } from './apiservice';
+import { Apiservice } from './apiservice'
 
 describe('Apiservice', () => {
   let service: Apiservice;
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+
 import { RegisterFooter } from "@/shared/register-footer/register-footer";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../apiservice';
+
 
 @Component({
   selector: 'app-register',
@@ -7,6 +11,49 @@ import { RegisterFooter } from "@/shared/register-footer/register-footer";
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
 
+
+
+
+
+export class Register implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private registerService: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      mobile: ['']      
+    });
+  }
+
+  sendOtp(): void {
+    if (this.registerForm.get('mobile')?.invalid) {
+      this.registerForm.get('mobile')?.markAsTouched();
+      return;
+    }
+
+    const mobile = this.registerForm.value.mobile;
+
+  /*  this.registerService.sendOtp(mobile).subscribe({
+      next: () => alert('OTP sent successfully'),
+      error: () => alert('Failed to send OTP')
+    });*/
+  }
+
+  onSubmit(): void {
+    alert("submit button clicked");
+    if (this.registerForm.invalid) return;
+
+    this.registerService.registerUser(this.registerForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        alert('Registration successful');
+      },
+      error: () => alert('Registration failed')
+    });
+  }
 }
