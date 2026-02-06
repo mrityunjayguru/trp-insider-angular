e773e6efcef9516410e638c8871e44cb5de94ff6
nirajniit <niit.delhi.ind@gmail.com>
2026-02-06 10:54:30 +0530

    commit

--- src/app/pages/business-setting/designation/designation.ts
+++ src/app/pages/business-setting/designation/designation.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../../../apiservice';
+
+
 
 interface DesignationItem {
   name: string;
@@ -12,8 +16,18 @@ interface DesignationItem {
   templateUrl: './designation.html',
   styleUrl: './designation.css',
 })
+
+
 export class Designation {
- departments: DesignationItem[] = [
+  formsize:any;
+  formBuilder: any;
+  apiService: ApiService;
+  sizes:any;
+  alldesignation:any;
+  logService : any;
+  allallcompany:any;
+
+departments: DesignationItem[] = [
     { name: 'Director', isActive: true },
     { name: 'Supervisor', isActive: true },
     { name: 'Foreman', isActive: false },
@@ -28,6 +42,86 @@ export class Designation {
     console.log('Edit department:', deg);
     // Add your edit logic here
   }
+
+
+  constructor(formBuilder: FormBuilder,   apiService: ApiService){
+    this.formBuilder=formBuilder;
+    this.apiService = apiService;
+    
+    this.formsize = this.formBuilder.group({
+      designationname:['',Validators.required],
+      
+
+    });
+      
+
+    this.apiService.getAllDesignation().subscribe(
+      (response : any) => {               
+        this.alldesignation = response.data;
+        console.log("alldesignation");
+        console.log(this.alldesignation);
+        console.log("alldesignation");
+          
+      })
+
+
+      
+
+
+  }
+
+  onSubmit(){
   
+  // var formData = new FormData();  
+
+  // formData.append("sizemasterdata", JSON.stringify(this.formsize.value));
+   
+  const payload = {
+    ...this.formsize.value,   // existing form fields
+    countrymasterobj: {
+      id: this.formsize.value.country_id                  // or this.formsize.value.companyId
+    }
+  };
+
+  const formData = new FormData();
+  formData.append(
+    'designationmasterData',
+    JSON.stringify(payload)
+  );
+   
+   this.apiService.saveDesignation(formData).subscribe(
+    (response : any) => {
+      
+      this.logService.clog(response,false);
+
+
+      if(response.status ==200)
+      {
+        alert("Size Added Successfully.");
+      }
+      else
+      {
+         alert("Not Add");
+      }
+    })
+
+
 }
+
+
+onCompanyChange(event: any) {
+  
+   const value = (event.target as HTMLSelectElement).value;
+
+  const company = this.allallcompany.find((c: { id: any; }) => c.id == value);
+  
+  
+  this.formsize.patchValue({
+    company_name: company ? company.companyName : ''
+  });
+}
+
+}
+
+
 
