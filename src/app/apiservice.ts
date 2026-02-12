49c39f4c17c0d17688feb7f77dd4140456677caa
nirajniit <niit.delhi.ind@gmail.com>
2026-02-12 14:41:06 +0530

    commit

--- src/app/apiservice.ts
+++ src/app/apiservice.ts
@@ -43,7 +43,7 @@ export class ApiService {
     // alert(" Niraj Updated "+this.apiUrl);
   
      
-     this.apiUrl = 'https://trpinsiderbackend.routerosterpro.com/api';
+    this.apiUrl = 'https://trpinsiderbackend.routerosterpro.com/api';
     //  this.apiUrl = 'http://localhost:8085/api';
         
        // alert(" Niraj Updated "+this.apiUrl);
